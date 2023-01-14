118f4799909f8b41158edfc0b5e895794e7971b1
Alan <aklt1994@gmail.com>
2023-01-14 17:01:13 +0100

    Create Utils

---
+++ scraping/utils.js
@@ -0,0 +1,17 @@
+export const URLS = {
+	leaderboard: 'https://kingsleague.pro/estadisticas/clasificacion/',
+	mvp: 'https://kingsleague.pro/estadisticas/mvp/'
+}
+
+export const cleanText = (text) =>
+	text
+		.replace(/\t|\n|\s:/g, '')
+		.replace(/.*:/g, ' ')
+		.trim()
+
+export async function scrape(url) {
+  const res = await fetch(url)
+  const html = await res.text()
+
+  return cheerio.load(html)
+}
